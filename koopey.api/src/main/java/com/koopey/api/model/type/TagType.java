3efd25993cf65530be363cafa8201c58c3fc8134
moleisking <moleisking@gmail.com>
2021-08-09 22:03:34 +0200

    add type

---
+++ koopey.api/src/main/java/com/koopey/api/model/type/TagType.java
@@ -0,0 +1,9 @@
+package com.koopey.api.model.type;
+
+public class TagType {
+    
+    public final static String ADULT = "adult";  
+    public final static String NORMAL = "normal";
+    public final static String POPULAR = "popular";  
+  
+}
