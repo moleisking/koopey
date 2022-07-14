3fcf384031d754504dccd5f9bf1b28ddf50e28ed
moleisking <moleisking@gmail.com>
2022-07-14 16:23:30 +0200

    add concourse

--- koopey.web/src/app/services/interceptors/authentication.interceptor.ts
+++ koopey.web/src/app/services/interceptors/authentication.interceptor.ts
@@ -2,19 +2,18 @@ import { catchError } from "rxjs/operators";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { NEVER, Observable, throwError } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
-    constructor(     ) {    }
+    constructor(private router: Router) { }
 
     public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(
             catchError(error => {
-                console.log("scan 401 error")
                 if (!!error.status && error.status === 401) {
-                    localStorage.removeItem("token");
-                    window.location.href =  "/login";
-                    console.log("got 401 error")
+                    localStorage.removeItem("token");                   
+                    this.router.navigate(["/login"]);
                     return NEVER;
                 }
                 return throwError(error);
